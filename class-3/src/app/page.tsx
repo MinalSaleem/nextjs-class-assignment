8d18f6e9bd99148f58369b08e2e8cc75bd5b3aaa
MinalSaleem <159961399+MinalSaleem@users.noreply.github.com>
2024-09-26 16:15:39 +0500

    Add files via upload

---
+++ class-3/src/app/page.tsx
@@ -0,0 +1,28 @@
+'use client'
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+    const route = useRouter()
+  return (
+    <div>
+
+        <div className="topdiv">
+          <button onClick={()=>route.push('/about')}>
+              About</button>
+
+          <button onClick={()=>route.push('/contact')}>
+              Contact</button>
+
+          <button onClick={()=>route.push('/footer')}>
+              Footer</button>
+        </div>
+  
+
+        <h1  style={{ color: 'navy', fontSize: '2rem' , margin: '80px' }}>This is Main Page of website.</h1>
+
+        <Link href={'navbar'}>Go to navbar</Link>
+
+    </div>
+  )
+};
