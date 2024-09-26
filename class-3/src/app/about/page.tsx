be605e308ca892ec72aab276f9d29f74ce91a5a9
MinalSaleem <159961399+MinalSaleem@users.noreply.github.com>
2024-09-26 15:34:57 +0500

    Add files via upload

---
+++ class-3/src/app/about/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "next/link";
+
+const About = () => {
+    return (
+        <>
+            <h1>This is About Page</h1>
+            <p>On this page, we share essential details about our business. </p>
+            <br />
+            <Link href="/">Go back Home</Link>
+        </>
+    )
+};
+
+export default About
