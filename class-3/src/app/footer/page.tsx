93f88acf40affab126e18c8a4393aebe2e36dc20
MinalSaleem <159961399+MinalSaleem@users.noreply.github.com>
2024-09-26 15:36:47 +0500

    Add files via upload

---
+++ class-3/src/app/footer/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Link from "next/link";
+
+const Footer = () => {
+    return (
+        <>
+            <h1>Footer Page</h1>
+            <br />
+            <Link href="/">Go back Home</Link>
+        </>
+    )
+};
+
+export default Footer;
