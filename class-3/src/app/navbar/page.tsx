a4b09dd751ac3e3706f36d0f01b6f99de17fefc9
MinalSaleem <159961399+MinalSaleem@users.noreply.github.com>
2024-09-26 15:37:50 +0500

    Add files via upload

---
+++ class-3/src/app/navbar/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Link from "next/link";
+
+const Navbar = () => {
+    return (
+        <>
+            <h1>Navbar Page</h1>
+            <br />
+            <Link href="/">Go back Home</Link> 
+        </>
+    )
+};
+
+export default Navbar;
